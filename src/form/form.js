7d13ad8a9ea17c1e335c4d99980aae1796505fe6
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2021-04-14 18:21:04 +0200

    first met

--- src/form/form.js
+++ src/form/form.js
@@ -1,2 +1,17 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const entries = formData.entries();
+
+  const obj = Array.from(entries).reduce((acc, value) => {
+    acc[value[0]] = value[1];
+    return acc;
+  }, {});
+  console.log(obj);
+});
