21c6550b06c2e3a0f129b09b77d57e82bfcb643c
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2021-04-14 18:47:07 +0200

    errors

--- src/form/form.js
+++ src/form/form.js
@@ -2,16 +2,31 @@ import "../assets/styles/styles.scss";
 import "./form.scss";
 
 const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
-
-  const entries = formData.entries();
-
-  const obj = Array.from(entries).reduce((acc, value) => {
-    acc[value[0]] = value[1];
-    return acc;
-  }, {});
-  console.log(obj);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    const json = JSON.stringify(article);
+  }
 });
+
+const formIsValid = (article) => {
+  if (!article.author || !article.category || !article.content) {
+    errors.push("Invalid");
+  } else {
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li> ${e} </li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+  } else {
+    errorElement.innerHTML = "";
+  }
+};
